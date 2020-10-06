1289119360100adf540566ff7c2beac3286bddc8
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-10-07 01:45:06 +0200

    zeloha pred zmenou md5

---
+++ cv3/cmake-build-debug/CMakeFiles/cv3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv3.dir/main.cpp.o"
+  "cv3"
+  "cv3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
