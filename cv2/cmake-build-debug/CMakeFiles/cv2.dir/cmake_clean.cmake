9312e76747d07844e8803a99bf98698376402c26
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-10-15 14:35:25 +0200

    se vsim vsudy komplet 2/10b

---
+++ cv2/cmake-build-debug/CMakeFiles/cv2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv2.dir/main.cpp.o"
+  "cv2"
+  "cv2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
