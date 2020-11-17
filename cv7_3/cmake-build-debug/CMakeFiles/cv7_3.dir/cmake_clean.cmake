ca16d9673634b6a76faf8a5469f0bca9104886ef
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-11-17 22:50:18 +0100

    nez to cele poseru

---
+++ cv7_3/cmake-build-debug/CMakeFiles/cv7_3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv7_3.dir/main.cu.o"
+  "CMakeFiles/cv7_3.dir/sauce.cu.o"
+  "CMakeFiles/cv7_3.dir/md5.cu.o"
+  "CMakeFiles/cv7_3.dir/cmake_device_link.o"
+  "cv7_3.pdb"
+  "cv7_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cv7_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
