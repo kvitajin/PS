933951234642828dd9f2148f4cd28ba892891dad
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-11-23 20:47:11 +0100

    i s binarkou

---
+++ cv6_arm/README.md
@@ -0,0 +1,6 @@
+![Lineární graf](https://imgur.com/mkNKWfo.png)  
+  
+![Logaritmický graf](https://imgur.com/C1ZzGGH.png) 
+  
+![Snapdragon 855+](https://imgur.com/pcNTTZ3.pngat :D
+) 
---
+++ cv6_arm/cmake-build-debug/CMakeFiles/cv6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv6.dir/main.cpp.o"
+  "CMakeFiles/cv6.dir/md5.cpp.o"
+  "cv6"
+  "cv6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
