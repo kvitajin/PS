321a53a6d5983194b8dd92aecda247233e6f009d
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-11-30 00:42:48 +0100

    slito

--- senHat/main.cpp
+++ senHat/main.cpp
@@ -1,11 +1,14 @@
 #include "HTS221-driver/HTS221.h"
 #include "LPS25H-driver/LPS25H.h"
 #include <iostream>
+#include <vector>
+#include <queue>
+
 
 int main(int argc, char** argv)
 {
     double humH, temH, preL, temL;
-
+    std::tuple<int, int, int, int> data;
     HTS221 humTem("/dev/i2c-2");
     LPS25H preTem("/dev/i2c-2");
     humTem.powerUp();
@@ -16,7 +19,9 @@ int main(int argc, char** argv)
     temH = humTem.getTemperature();
     preL = preTem.getPressure();
     temL = preTem.getTemperature();
-    
-    std::cout<< humH << " " << temH <<" " << preL << " " << temL<<std::endl;	
+    data= std::make_tuple(humTem.getHumidity(),
+                            humTem.getTemperature(),
+                            preTem.getPressure(),
+                            preTem.getTemperature());
     return 0;
 }
